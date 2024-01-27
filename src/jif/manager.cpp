9381d55c2c0c9e2b336f6c4e6a5a13671f7f62fc
Felix Schreiber <f.schreiber.2000@web.de>
2024-01-27 15:05:19 +0100

    added missing license headers

--- src/jif/manager.cpp
+++ src/jif/manager.cpp
@@ -1,3 +1,13 @@
+/*
+ * ------------------------------------------------------------
+ * "THE BEERWARE LICENSE" (Revision 42):
+ * Felix Schreiber wrote this code. As long as you retain this
+ * notice, you can do whatever you want with this stuff. If we
+ * meet someday, and you think this stuff is worth it, you can
+ * buy me a beer in return.
+ * ------------------------------------------------------------
+ */
+
 #include <fstream>
 #include <imgui/imgui.h>
 #include <imgui/imgui_internal.h>
